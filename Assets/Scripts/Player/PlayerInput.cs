0e541000beffd5715d2d22b2d5ffd9603b472ce1
Jupo9 <JulianP96@gmx.de>
2025-12-23 04:40:31 +0100

    ADD, CHANGE
    ADD:
    1. Camera zoom snapping
    
    CHANGE:
    1. PlayerInput.cs

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,38 +1,87 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+
     [SerializeField] private float keyboardPanSpeed = 5f;
+    [SerializeField] private float zoomSpeed = 1f;
+    [SerializeField] private float minZoomDistance = 7.5f;
+
+    private CinemachineFollow cinemachineFollow;
+    private float zoomStartTime;
+    private Vector3 startingFollowOffset;
+
+    private void Awake()
+    {
+       //cinemachineFollow = cinemachineCamera.GetComponent<CinemachineFollow>();
+       if (!cinemachineCamera.TryGetComponent(out cinemachineFollow))
+        {
+            Debug.LogError("Cinemachine Camera did not have CinemachineFollow component. Zoom will not work!");
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
 
     private void Update()
     {
         CameraMovement();
+        CameraZoom();
     }
 
     private void CameraMovement()
     {
         Vector2 moveAmount = Vector2.zero;
 
-        if (Keyboard.current.upArrowKey.isPressed)
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed)
         {
             moveAmount.y += keyboardPanSpeed;
         }
-        if (Keyboard.current.leftArrowKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
         {
             moveAmount.x -= keyboardPanSpeed;
         }
-        if (Keyboard.current.downArrowKey.isPressed)
+        if (Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed)
         {
             moveAmount.y -= keyboardPanSpeed;
         }
-        if (Keyboard.current.rightArrowKey.isPressed)
+        if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
         {
             moveAmount.x += keyboardPanSpeed;
         }
 
         moveAmount *= Time.deltaTime;
         cameraTarget.position += new Vector3(moveAmount.x, 0, moveAmount.y);
+    }
+
+    private void CameraZoom()
+    {
+        if (ShouldZoomTimeStart())
+        {
+            zoomStartTime = Time.time;
+        }
+
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+        Vector3 targetFollowOffset;
+
+        if (Keyboard.current.endKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(cinemachineFollow.FollowOffset.x, minZoomDistance, cinemachineFollow.FollowOffset.z);
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(cinemachineFollow.FollowOffset.x, startingFollowOffset.y, cinemachineFollow.FollowOffset.z);
+            cinemachineFollow.FollowOffset = Vector3.Slerp(targetFollowOffset, startingFollowOffset, zoomTime);
+        }
+
+        cinemachineFollow.FollowOffset = Vector3.Slerp(cinemachineFollow.FollowOffset, targetFollowOffset, zoomTime);
+    }
+
+    private bool ShouldZoomTimeStart()
+    {
+        return Keyboard.current.endKey.wasPressedThisFrame || Keyboard.current.endKey.wasReleasedThisFrame;
     }
 }
